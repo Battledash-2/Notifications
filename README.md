0d36f46d8026c97a51e60188364407229faabdb1
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2023-08-15 21:16:08 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,64 @@
+###### No name yet 😥
+
+# Notifications
+
+Notifications is a customizable, appealing, and stylistic notification system.<br/>
+Using [Fontawesome](https://fontawesome.com/), you can use tons of clean icons.
+
+### Usage
+
+To use Notifications, first **import** the stylesheets in the head, like this:
+
+```html
+<link rel="stylesheet" href="?" />
+```
+
+To send notifications, **import** the file using the ES6 `import` syntax. Remember that you can only import scripts within modules, so on your script tag you have to use `type="module"`!
+
+```html
+<script type="module">
+	import * as Notifications from '?';
+
+	Notifications.ShowNotification(null, 'warning', Infinity, 'Hello!', 'INFO');
+</script>
+```
+
+### API
+
+Notifications will be referred to as `API`.
+
+> ### `API.remove(elm: HTMLElement): void;`
+
+-   Removes notification `elm` from the screen, while still showing the transition out.
+
+> ### `API.ShowNotification(icon = 'fa-regular fa-bell', color = 'info', duration = 5000, text = 'Unknown', caption = 'Unknown', closable = true): HTMLElement;`
+
+-   Creates a new notifications and adds it to the screen.<br/> _Returns the created element._
+
+> ### `API.NotifExists(icon: string, color: string, text: string, caption: string)`
+
+-   Checks if a notifications with the provided properties exists.<br/>_Returns null or the element found._
+
+#### Configuration
+
+> ### `function Max(max?: number)`
+
+-   Sets the **max amount of notifications** on the screen at once if provided.<br />_Returns the new max._
+
+> ### `function CounterMax(max?: number)`
+
+-   Sets the **max amount** for the small counter if provided.<br />_Returns the new counter max._
+
+> ### `function Icons(icns?: Object)`
+
+-   Sets the default icons object if provided. If the key is also in **Colors** and the _color_ for `API.ShowNotification` is a property of **Colors**, it will set the icon to a the corresponding value here.<br />_Returns the new icons object._
+
+> ### `function Colors(clrs?: Object)`
+
+-   Sets the default colors object if provided. If colors has a property, it should also be in `Icons`. If you provide a key of **Colors** into the _color_ for `API.ShowNotification`, it will set it to the value from this object, along with setting the Icon to the value from the `Icons` object.<br />_Returns the new colors object._
+
+## You can find an example in `./test/index.html`! 🧪
+
+Pictures:
+![example picture](https://raw.githubusercontent.com/Battledash-2/Notifications/main/screenshots/screenshot0.png)
+![example picture](https://raw.githubusercontent.com/Battledash-2/Notifications/main/screenshots/screenshot1.png)
---
+++ src/script.js
@@ -0,0 +1,168 @@
+// when multiple of the same notifications are made, it deletes the old one and counts up the new one
+// false = disabled
+// true = enabled
+// will be ignored if also declared within config.lua
+const COUNTER_ATTR_NAME = 'data-amount'; // DO NOT CHANGE!
+let COUNTER = true;
+
+let COUNTER_MAX = 99;
+let MAX = 4;
+
+export function Max(max) {
+	if (max != null) MAX = max;
+	return MAX;
+}
+export function CounterMax(max) {
+	if (max != null) COUNTER_MAX = max;
+	return COUNTER_MAX;
+}
+
+let icons = {
+	info: 'fa-solid fa-circle-info',
+	primary: 'fa-solid fa-circle-info',
+	success: 'fa-solid fa-check',
+	error: 'fa-solid fa-triangle-exclamation',
+	warning: 'fa-solid fa-triangle-exclamation',
+};
+let colors = {
+	info: '#333333',
+	primary: '#333333',
+	success: '#72cf30',
+	error: '#d44c31',
+	warning: '#d9d74c',
+};
+
+export function Colors(clrs) {
+	if (clrs != null) colors = clrs;
+	return colors;
+}
+export function Icons(icns) {
+	if (icns != null) icons = icns;
+	return icons;
+}
+
+export function Counter(ctr) {
+	if (ctr != null) COUNTER = ctr;
+	return COUNTER;
+}
+
+/*
+	action = 'notify',
+	type = ttype or 'primary',
+	duration = length,
+	text = text,
+	caption = capt,
+*/
+
+/**
+ * @param {HTMLElement} elm
+ * @param {string} classList
+ */
+function $c(elm, classList = '') {
+	const e = document.createElement(elm);
+	e.className = classList;
+	return e;
+}
+// const cont = document.querySelector('#notif-container');
+const cont = document.querySelector('#__notif-container') || $c('div');
+cont.id = '__notif-container';
+document.body.appendChild(cont);
+
+/**
+ * @param {HTMLElement} elm
+ */
+export function remove(elm) {
+	elm.classList.remove('active');
+	setTimeout(() => elm.remove(), (parseFloat(getComputedStyle(elm).transitionDuration) * 1000) / 1.333333333);
+}
+/**
+ * @param {HTMLElement} elm
+ * @param {number} duration
+ * @param {Function?} cb
+ */
+function setupForRemoval(elm, duration, cb) {
+	setTimeout(() => {
+		if (elm.parentNode) {
+			remove(elm);
+			if (cb && typeof cb === 'function') cb();
+		}
+	}, duration);
+}
+
+/**
+ * @param {"info"|"primary"|"success"|"warning"|"error"} icon
+ * @param {"info"|"primary"|"success"|"warning"|"error"} color
+ * @param {number|Infinity} duration
+ * @param {string} text
+ * @param {string} caption
+ * @param {boolean} closable
+ * @returns {HTMLElement}
+ */
+export function ShowNotification(icon = 'fa-regular fa-bell', color = 'info', duration = 5000, text = 'Unknown', caption = 'Unknown', closable = true) {
+	if (colors.hasOwnProperty(color)) {
+		icon = icons[color] || icon;
+		color = colors[color] || color;
+	}
+
+	let notif = $c('div', 'notif');
+	notif.style.backgroundColor = color;
+	notif.style.boxShadow = '0 0 1.563rem 0 ' + color;
+
+	if (COUNTER) {
+		const notifExists = NotifExists(icon, notif.style.backgroundColor, text, caption);
+		if (notifExists) {
+			notif.setAttribute(COUNTER_ATTR_NAME, Math.min(notifExists.hasAttribute(COUNTER_ATTR_NAME) ? parseInt(notifExists.getAttribute(COUNTER_ATTR_NAME)) + 1 : 2, COUNTER_MAX));
+			remove(notifExists);
+		}
+	}
+
+	if (cont.childElementCount >= MAX) {
+		if (cont.lastElementChild) {
+			remove(cont.lastElementChild);
+		}
+	}
+
+	let h3 = $c('h3');
+	if (icon) {
+		let ic = $c('span', icon);
+		h3.appendChild(ic);
+	}
+	h3.append(caption);
+	notif.appendChild(h3);
+
+	let p = $c('p');
+	p.textContent = text;
+
+	notif.appendChild(p);
+
+	cont.prepend(notif);
+	setTimeout(() => notif.classList.add('active'), 15);
+
+	if (duration !== Infinity && duration >= 0) setupForRemoval(notif, duration <= 0 ? 1000 : duration);
+
+	if (closable) {
+		notif.classList.add('clickable');
+		notif.addEventListener('click', (_e) => {
+			remove(notif);
+		});
+	}
+
+	return notif;
+}
+
+/** @returns {HTMLElement?} */
+export function NotifExists(icon, color, text, caption) {
+	let res = null;
+	Array.from(cont.querySelectorAll('.notif.active')).some((notif) => {
+		if (!notif.style.backgroundColor.includes(color)) return;
+		const head = notif.querySelector('h3');
+		const iicon = head.querySelector('span');
+		if ((icon && !iicon) || (!icon && iicon)) return;
+		if (icon && iicon.className !== icon) return;
+		if (head.innerText.toLowerCase() !== caption.toLowerCase()) return;
+		if (notif.querySelector('p').innerText.toLowerCase() !== text.toLowerCase()) return;
+		res = notif;
+		return true;
+	});
+	return res;
+}
